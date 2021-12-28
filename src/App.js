ea44db721f4141ba362fd3f637552b2fc3b466c8
Oksana <ksenia79460@gmail.com>
2021-12-28 15:38:02 +0000

    create new component Dictionary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">HELLO</header>
+        <header className="App-header">Dictionary</header>
+        <main className="App-main">
+        <Dictionary />
+          
+        </main>
+        <footer className="App-footer">Coded by Oksana</footer>
       </div>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import { fireEvent } from "@testing-library/react";
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function changeKeyword(event) {
+    //console.log(event.target.value);
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={changeKeyword} />
+      </form>
+    </div>
+  );
+}
