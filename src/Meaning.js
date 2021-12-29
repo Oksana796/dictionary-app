f14ad02763ba93413d8deb8c6076d29feba82dc4
Oksana <ksenia79460@gmail.com>
2021-12-29 12:06:43 +0000

    add synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.mean);
@@ -10,10 +11,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
-              <br></br>
-
+              <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+
+              <Synonyms synon={definition.synonyms} />
             </p>
           </div>
         );
---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Synonims(props) {
+  console.log(props.synon);
+  if (props.synon) {
+    return (
+      <ul className="Synonyms">
+        {props.synon.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
